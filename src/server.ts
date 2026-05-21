c00ce7c69d6cf2a94ae94517c070a7eaaa2e6096
ryan <mr.yaryyev@gmail.com>
2026-05-22 04:34:33 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -18,3 +18,12 @@ console.log("MONGO_URL:", process.env.MONGO_URL);
 
 // CLUSTER => DATABASE => COLLECTION => DOCUMENT
 // distributed database = NoSql
+
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log('MongoDB connection succeed');
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch(err => console.log('ERROR on connection MongoDB', err));
