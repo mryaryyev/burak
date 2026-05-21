5dcdd079feb73c43479233c483b359ae0d7b83fa
ryan <mr.yaryyev@gmail.com>
2026-05-22 05:08:02 +0900

    feat: express integrations

--- src/server.ts
+++ src/server.ts
@@ -12,18 +12,23 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("PORT:", process.env.PORT);
+//console.log("PORT:", process.env.PORT);
 
-console.log("MONGO_URL:", process.env.MONGO_URL);
+//console.log("MONGO_URL:", process.env.MONGO_URL);
 
 // CLUSTER => DATABASE => COLLECTION => DOCUMENT
 // distributed database = NoSql
 
 import mongoose from "mongoose";
+import app from "./app";
+
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
-    console.log('MongoDB connection succeed');
+    console.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.log(`The server is running successfully on port: ${PORT}`);
+    });
   })
-  .catch(err => console.log('ERROR on connection MongoDB', err));
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
