e55020ba44e811d5e36519bb7617fcccd267629a
ryan <mr.yaryyev@gmail.com>
2026-05-22 04:09:23 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,13 @@
 // Design pattern: Middleware, Decodar
 
 // Design pattern -> 3 categories -> Creational, Structural and Behavioral
+
+// ESM JS => import moment from "moment";
+// COMMON JS => const moment = require('moment');
+
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log("PORT:", process.env.PORT);
+
+console.log("MONGO_URL:", process.env.MONGO_URL);
