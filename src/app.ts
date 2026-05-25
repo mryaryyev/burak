044a34959ba8f9f4144c88754654ab4354bef3c6
ryan <mr.yaryyev@gmail.com>
2026-05-25 17:00:10 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -15,6 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
+app.use("/", router); // Middleware Design Pattern
 
 export default app; // ESJS module JS
 // module.exports = app; => COMMON JS
