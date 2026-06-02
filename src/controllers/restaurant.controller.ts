5be104444262ed02e971ff1e7a92dbc0fe2487cc
ryan <mr.yaryyev@gmail.com>
2026-06-02 19:03:51 +0900

    fix: modify restaurant login and signup process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -43,7 +43,7 @@ restaurantController.processLogin = async (req: Request, res: Response) => {
     const result = await memberService.processLogin(input);
 
     res.send(result);
-  } catch (err) {
+  } catch (err) { 
     console.log("Error, processLogin:", err);
     res.send(err);
   }
