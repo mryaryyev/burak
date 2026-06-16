053ae5238c6d37413bc9e2fa54025705889368fd
ryan <mr.yaryyev@gmail.com>
2026-06-16 22:33:22 +0900

    feat: develop updateChosenUser api

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,8 +1,14 @@
 import MemberModel from "../schema/Member.model";
-import { MemberInput, Member, LoginInput } from "../libs/types/member";
+import {
+  MemberInput,
+  Member,
+  LoginInput,
+  MemberUpdateInput,
+} from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
+import { shapeIntoMongooseObjectId } from "../libs/config";
 
 class MemberService {
   private readonly memberModel;
@@ -99,6 +105,17 @@ class MemberService {
       .exec();
 
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
+
+  public async updateChosenUser(input: MemberUpdateInput): Promise<Member> {
+    input._id = shapeIntoMongooseObjectId(input._id);
+    const result = await this.memberModel
+      .findByIdAndUpdate({ _id: input._id }, input, { new: true })
+      .exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
 
     return result;
   }
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -45,4 +45,10 @@ routerAdmin.get(
   restaurantController.getUsers,
 );
 
+routerAdmin.post(
+  "/user/edit",
+  restaurantController.verifyRestaurant,
+  restaurantController.updateChosenUser,
+);
+
 export default routerAdmin;
