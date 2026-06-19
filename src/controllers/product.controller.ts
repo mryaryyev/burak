3954b84c71f32963a04e4c1a5565b4e57d91bf7d
ryan <mr.yaryyev@gmail.com>
2026-06-19 11:27:13 +0900

    feat: create new product frontend logic develop

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -30,6 +30,7 @@ productController.createNewProduct = async (
 ) => {
   try {
     console.log("createNewProduct");
+    console.log("req.body:", req.body);
     if (!req.files?.length)
       throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
@@ -40,14 +41,14 @@ productController.createNewProduct = async (
 
     await productService.createNewProduct(data);
     res.send(
-      `<script> alert("Successfully creation"); window.location.replace('admin/product/all') </script>`,
+      `<script> alert("Successfully creation"); window.location.replace('/admin/product/all') </script>`,
     );
   } catch (err) {
     console.log("Error, createNewProduct:", err);
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}"); window.location.replace('admin/product/all') </script>`,
+      `<script> alert("${message}"); window.location.replace('/admin/product/all') </script>`,
     );
   }
 };
