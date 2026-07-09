fec1ec2939f609914407f70d8615b45131b4732a
ryan <mr.yaryyev@gmail.com>
2026-07-09 21:13:07 +0900

    feat: develop getProducts rest Api

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -2,10 +2,13 @@ import ProductModel from "../schema/Product.model";
 import {
   Product,
   ProductInput,
+  ProductInquiry,
   ProductUpdateInput,
 } from "../libs/types/product";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { shapeIntoMongooseObjectId } from "../libs/config";
+import { T } from "../libs/types/common";
+import { ProductStatus } from "../libs/enums/product.enum";
 
 class ProductService {
   private readonly productModel;
@@ -14,6 +17,34 @@ class ProductService {
   }
 
   /* SPA */
+
+  public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
+    //console.log("inquiry: ", inquiry);
+    const match: T = { productStatus: ProductStatus.PROCESS };
+
+    if (inquiry.productCollection)
+      match.productCollection = inquiry.productCollection;
+    if (inquiry.search) {
+      match.productName = { $regex: new RegExp(inquiry.search, "i") };
+    }
+
+    const sort: T =
+      inquiry.order === "productPrice"
+        ? { [inquiry.order]: 1 }
+        : { [inquiry.order]: -1 };
+
+    const result = await this.productModel
+      .aggregate([
+        { $match: match },
+        { $sort: sort },
+        { $skip: (inquiry.page * 1 - 1) * inquiry.limit },
+        { $limit: inquiry.limit * 1 },
+      ])
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
 
   /* SSR */
 
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader";
+import productController from "./controllers/product.controller";
 
 /** Member */
 router.get("/member/restaurant", memberController.getRestaurant);
@@ -29,7 +30,8 @@ router.post(
 router.get("/member/top-users", memberController.getTopUsers);
 
 /** Product */
-router.get("/product/");
+router.get("/product/all", productController.getProducts);
+// router.get("/product/all/:key/:id", productController.getProducts);
 
 /** Order */
 router.get("/order/");
